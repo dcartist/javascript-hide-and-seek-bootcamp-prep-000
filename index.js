ffbe213ba516258c1950d946e11848c7fc489159
dcartist <pbannerman13@gmail.com>
2018-06-12 01:08:28 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,20 @@
+function getFirstSelector(selector){
+  var firstone = document.querySelector(selector)
+  return firstone
+}
+function deepestChild(){
+var deep = document.querySelectorAll('#grand-node div')
+return deep[3]
+}
+function nestedTarget(){
+  var nested = document.querySelector('.target')
+  return nested
+}
+function increaseRankBy(n){
+  var count = document.querySelectorAll('.ranked-list li')
+  for (var i = count.length - 1; 0 <= i; i--){
+    var x = parseInt(count[i].innerText)
+    count[i].innerText = n + x
+  }
+  // return count
+}
