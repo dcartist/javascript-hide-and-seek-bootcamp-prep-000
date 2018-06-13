9d774bdb97aaa4f3dbee4a494c63323f2c749f51
dcartist <pbannerman13@gmail.com>
2018-06-13 19:25:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,13 @@ function nestedTarget(){
   var nested = document.querySelector('.target')
   return nested
 }
-function increaseRankBy(n){
-  var count = document.querySelectorAll('.ranked-list li')
-  for (var i = count.length - 1; 0 <= i; i--){
-    var x = parseInt(count[i].innerText)
-    count[i].innerText = n + x
-  }
-  // return count
-}
+// function increaseRankBy(n){
+//   var count = document.querySelectorAll('.ranked-list li')
+//   for (var i = count.length - 1; 0 <= i; i--){
+//     var x = parseInt(count[i].innerText)
+//     count[i].innerText = n + x
+//   }
+//   // return count
+// }
+
+
