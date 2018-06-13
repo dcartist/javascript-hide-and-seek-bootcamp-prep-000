1bcc7870c4e7d782c360d48f46bbf5654ded7e47
dcartist <pbannerman13@gmail.com>
2018-06-13 19:25:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,5 +18,14 @@ function nestedTarget(){
 //   }
 //   // return count
 // }
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll('.ranked-list')
 
+  for (var i = 0, l = rankedLists.length; i < l; i++) {
+    var children = rankedLists[i].children
 
+    for (var j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
+  }
+}
