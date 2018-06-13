11ed6fa825d4d81a477069feeffb662f4379f017
dcartist <pbannerman13@gmail.com>
2018-06-13 19:26:20 +0000

    Done.

--- index.js
+++ index.js
@@ -10,11 +10,22 @@ function nestedTarget(){
   var nested = document.querySelector('.target')
   return nested
 }
-function increaseRankBy(n){
-  var count = document.querySelectorAll('.ranked-list li')
-  for (var i = count.length - 1; 0 <= i; i--){
-    var x = parseInt(count[i].innerText)
-    count[i].innerText = n + x
+// function increaseRankBy(n){
+//   var count = document.querySelectorAll('.ranked-list li')
+//   for (var i = count.length - 1; 0 <= i; i--){
+//     var x = parseInt(count[i].innerText)
+//     count[i].innerText = n + x
+//   }
+//   // return count
+// }
+function increaseRankBy(n) {
+  var rankedLists = document.querySelectorAll('.ranked-list')
+
+  for (var i = 0, l = rankedLists.length; i < l; i++) {
+    var children = rankedLists[i].children
+
+    for (var j = 0, k = children.length; j < k; j++) {
+      children[j].innerHTML = parseInt(children[j].innerHTML) + n
+    }
   }
-  // return count
 }
